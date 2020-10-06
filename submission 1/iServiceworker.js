62596d920f17bc7b36706ba0694c7af03e0d58ff
ikhzz <fikriztm@gmail.com>
2020-10-06 15:07:09 +0800

    fix manifest and cache ?. submit r-5

--- submission 1/iServiceworker.js
+++ submission 1/iServiceworker.js
@@ -28,7 +28,10 @@ var urlsToCache = [
   "img/assets/therental.jpg",
   "https://fonts.googleapis.com/icon?family=Material+Icons",
   "https://fonts.gstatic.com/s/materialicons/v55/flUhRq6tzZclQEJ-Vdg-IuiaDsNc.woff2",
-  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff2?v=4.7.0",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.woff?v=4.7.0",
+  "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/fonts/fontawesome-webfont.ttf?v=4.7.0"
 ];
  
 self.addEventListener("install", function(event) {
